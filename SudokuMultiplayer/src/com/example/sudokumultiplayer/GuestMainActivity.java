89cf964323d218280e68dd2da80630a700c17a16
Alex Reiff <areiff19@gmail.com>
2015-01-22 15:30:25 -0500

    added some dummy screens

--- SudokuMultiplayer/src/com/example/sudokumultiplayer/GuestMainActivity.java
+++ SudokuMultiplayer/src/com/example/sudokumultiplayer/GuestMainActivity.java
@@ -2,25 +2,23 @@ package com.example.sudokumultiplayer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class GuestMainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_guest_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_guest_main, menu);
         return true;
     }
 
@@ -30,24 +28,12 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
-    }
-
-    public void loginButtonPress(View view) {
-        Intent login_intent = new Intent(this, LoginActivity.class);
-        startActivity(login_intent);
-    }
-
-    public void registerButtonPress(View view) {
-        Intent register_intent = new Intent(this, RegisterActivity.class);
-        startActivity(register_intent);
-    }
-
-    public void guestButtonPress(View view) {
-        Intent guest_intent = new Intent(this, GuestMainActivity.class);
-        startActivity(guest_intent);
     }
 }
--- SudokuMultiplayer/src/com/example/sudokumultiplayer/LoginActivity.java
+++ SudokuMultiplayer/src/com/example/sudokumultiplayer/LoginActivity.java
@@ -2,52 +2,38 @@ package com.example.sudokumultiplayer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
-
+*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
-    }
-
-    public void loginButtonPress(View view) {
-        Intent login_intent = new Intent(this, LoginActivity.class);
-        startActivity(login_intent);
-    }
-
-    public void registerButtonPress(View view) {
-        Intent register_intent = new Intent(this, RegisterActivity.class);
-        startActivity(register_intent);
-    }
-
-    public void guestButtonPress(View view) {
-        Intent guest_intent = new Intent(this, GuestMainActivity.class);
-        startActivity(guest_intent);
     }
 }
