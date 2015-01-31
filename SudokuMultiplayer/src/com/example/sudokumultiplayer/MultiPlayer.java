d6297870c843df2c52fb783a26f4c3893d3c6c11
Alex Reiff <areiff19@gmail.com>
2015-01-31 14:13:30 -0500

    all screens added, still super ugly

--- SudokuMultiplayer/src/com/example/sudokumultiplayer/MultiPlayer.java
+++ SudokuMultiplayer/src/com/example/sudokumultiplayer/MultiPlayer.java
@@ -1,26 +1,24 @@
 package com.example.sudokumultiplayer;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class GuestMainActivity extends ActionBarActivity {
+public class MultiPlayer extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guest_main);
+        setContentView(R.layout.activity_multi_player);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_guest_main, menu);
+        getMenuInflater().inflate(R.menu.menu_multi_player, menu);
         return true;
     }
 
@@ -37,20 +35,5 @@ public class GuestMainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void singlePlayerButtonPress(View view) {
-        Intent single_player_intent = new Intent(this, SinglePlayer.class);
-        startActivity(single_player_intent);
-    }
-
-    public void multiPlayerButtonPress(View view) {
-        Intent multiplayer_intent = new Intent(this, MultiPlayer.class);
-        startActivity(multiplayer_intent);
-    }
-
-    public void viewStatsButtonPress(View view) {
-        Intent view_stats_intent = new Intent(this, ViewStatsActivity.class);
-        startActivity(view_stats_intent);
     }
 }
