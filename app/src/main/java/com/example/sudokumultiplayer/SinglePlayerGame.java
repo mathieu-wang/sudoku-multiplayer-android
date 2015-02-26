4c2d09c559740371982e55069ee9f1040cc390b8
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-02-26 13:02:34 -0500

    Add mock sudoku string and format helper.

--- app/src/main/java/com/example/sudokumultiplayer/SinglePlayerGame.java
+++ app/src/main/java/com/example/sudokumultiplayer/SinglePlayerGame.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -65,7 +66,24 @@ public class SinglePlayerGame extends ActionBarActivity {
 
     public void startGame (View view) {
         System.out.println("Starting game with difficulty: " + difficulty.name());
+        String sudokuString = generateSudokuString();
+        String[] numbers = sudokuString.split(",");
         
+    }
+
+    private String generateSudokuString() {
+        String sudokuJsonString =  "{\n" +
+                "    \"sudoku\": \"8,6,0,9,0,0,0,4,3,9,0,0,2,0,3,8,6,1,0,4,3,0,6,1,9,7,0,0,0,9,1,5,0,4,3,0,0,0,7,4,3,0,0,8,0,4,3,2,6,8,9,1,0,7,0,1,0,0,9,6,3,0,4,0,9,6,0,0,4,7,1,8,0,0,0,7,1,8,5,0,0\"\n" +
+                "}\n";
+
+        String sudokuString = "";
+        String[] tokens = sudokuJsonString.split("\"");
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("sudoku")) {
+                sudokuString = tokens[i + 2];
+            }
+        }
+        return sudokuString;
     }
 
     public void leaveGameButtonPress(View view) {
